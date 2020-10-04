83c569c92661165c7f607506c8bcf530e37377d2
fffengmjl <m781131011@outlook.com>
2020-10-04 22:26:42 +0800

    add: 完成除了牧师魔鬼之外的作业

---
+++ 003/SolarSystem/Assets/Scripts/Revolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolution : MonoBehaviour
+{
+  public Transform center; // 公转中心，需要设置
+  private float speed; // 速度
+  private float x; // 用于设置法平面
+  private float y; // 用于设置法平面
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    speed = Random.Range(9, 12);
+    x = Random.Range(-50, 50);
+    y = Random.Range(-50, 50);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var axis = new Vector3(0, x, y);
+    transform.RotateAround(center.position, axis, speed * Time.deltaTime);
+  }
+}
---
+++ 003/SolarSystem/Assets/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+  // Update is called once per frame
+  void Update()
+  {
+    transform.RotateAround(transform.position, Vector3.up, Random.Range(1, 3));
+  }
+}
